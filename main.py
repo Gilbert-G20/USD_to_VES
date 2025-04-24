1a8cb4f20642f44012aaad748d08923138042232
Gilbert Gomez <gomeznarvaezgilbert@gmail.com>
2025-04-24 11:16:33 -0400

    This module receives data from get_bcv.py and get_paralell.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import datetime
+from fastapi import FastAPI
+from get_paralell import get_usd_ves_rateBCV
+from get_bcv import get_usd_ves_ratePL
+app = FastAPI()
+
+@app.get("/api/rate")
+def get_rateBCV():  
+    data = get_usd_ves_rateBCV()  
+    return {
+        "currency": "USD-BCV",
+        "to": "VES",
+        "rate": data.get("rate", "N/A"),
+        "updated_at": datetime.utcnow().isoformat()
+    }
+
+def get_ratePL():  
+    data = get_usd_ves_ratePL()  
+    return {
+        "currency": "USD-Parallel",
+        "to": "VES",
+        "rate": data.get("rate", "N/A"),
+        "updated_at": datetime.utcnow().isoformat()
+    }
